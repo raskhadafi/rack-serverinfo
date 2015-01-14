5e2974359198a49deb44d8736801827a9ed5a26d
Roman Simecek <roman@good2go.ch>
2015-01-14 16:04:13 +0100

    Show the content of the database.yml when existing.

--- lib/rack/serverinfo.rb
+++ lib/rack/serverinfo.rb
@@ -5,13 +5,27 @@ module Rack
 
   class Serverinfo < Plastic
 
+    DatabaseYamlPath = 'config/database.yml'
+
     def change_nokogiri_doc(doc)
       return doc if request.xhr?
 
-      badge = doc.create_element 'div', style: style
+      badge = doc.create_element 'div', style: badge_style
 
-      server_info_text.each do |info|
-        badge.add_child(doc.create_element('p', info, style: 'margin: 0'))
+      badge.add_child(doc.create_element('pre', server_info_text, style: pre_style))
+
+      if db_connection_config.empty?
+        badge.add_child(doc.create_element('pre', "DB: database.yml does not exist", style: pre_style))
+      else
+        db_detail_container = doc.create_element 'details'
+
+        db_detail_container.add_child(doc.create_element('summary', "DB: #{db_connection_config['database']}"))
+
+        db_connection_config.each do |key, value|
+          db_detail_container.add_child(doc.create_element('p', "#{key}: #{value}", style: "padding-left: 9.5px;margin: 0;"))
+        end
+
+        badge.add_child(db_detail_container)
       end
 
       append_to_body(doc, badge)
@@ -23,25 +37,53 @@ module Rack
 
     def server_info_text
       @server_info_text ||= [
-        "Hostname: #{hostname}",
-        "User: #{user.name}",
-        "Rails-Env: #{rails_env}",
-      ]
+        "Hostname:  #{hostname}",
+        "User:      #{user.name}",
+        "RAILS_ENV: #{rails_env}",
+      ].join("\n")
     end
 
-    def style
-      @style ||= [
-        'position: absolute;',
-        'bottom: 10px;',
-        'right: 10px;',
-        'border: 4px solid red;',
-        'padding: 5px;',
-        'border-radius: 5px;',
-        'background-color: black;',
-        'opacity: 0.5;',
-        'color: white;',
-        'font-size: 10px;',
-      ].join
+    def pre_style
+      @pre_style ||= <<-pre_style
+display: block;
+padding: 0 0 0 9.5px;
+margin: 0;
+font-size: 10px;
+line-height: 12px;
+word-break: break-all;
+word-wrap: break-word;
+white-space: pre;
+white-space: pre-wrap;
+background-color: black;
+pre_style
+    end
+
+    def badge_style
+      @badge_style ||= [
+        'position: absolute',
+        'bottom: 10px',
+        'right: 10px',
+        'border: 4px solid red',
+        'padding: 5px',
+        'border-radius: 5px',
+        'background-color: black',
+        'opacity: 0.5',
+        'color: white',
+        'font-size: 10px',
+        'font-family: monospace',
+      ].join(';')
+    end
+
+    def db_name
+      @db_name ||= db_connection_config['database']
+    end
+
+    def db_connection_config
+      @db_connection_config ||= if ::File.file?(DatabaseYamlPath)
+        YAML.load_file(DatabaseYamlPath)[rails_env]
+      else
+        {}
+      end
     end
 
     def user
@@ -49,7 +91,7 @@ module Rack
     end
 
     def rails_env
-      @rails_env ||= ENV['RAILS_ENV']
+      @rails_env ||= (ENV['RAILS_ENV'] || 'not set')
     end
 
     def hostname
